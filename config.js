dda2b69d956767be7a5aac322eee8bea93118e6d
12Lraka <98644795+12Lraka@users.noreply.github.com>
2026-01-11 08:25:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-window.SUPABASE_URL = "https://YOUR-PROJECT.supabase.co";
-window.SUPABASE_ANON_KEY = "YOUR-ANON-KEY";
+window.SUPABASE_URL = "https://sbwmjnuccroesviatxpc.supabase.co";
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNid21qbnVjY3JvZXN2aWF0eHBjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgwNzMxODAsImV4cCI6MjA4MzY0OTE4MH0.TDjXz7jK7JjX0nDols1CJbsIa4UDz3Lfn6YfLDcA_w8";
 window.ADMIN_EMAIL = "admin@gmail.com";
 window.ADMIN_PASSWORD = "25voting";
 window.VOTE_TIME_MINUTES = 10;
